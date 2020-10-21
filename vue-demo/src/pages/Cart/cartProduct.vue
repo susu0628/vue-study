62d7e6e7f21a64e185a5de05e6003bd380ed7d17
jiangsusu <jiangsusu7586@dingtalk.com>
2020-10-21 15:41:03 +0800

    购物车页面初步完成

---
+++ vue-demo/src/pages/Cart/cartProduct.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="car_product">
+    <VantCheckbox v-model="checked" />
+    <div class="car_box">
+      <div class="car_left">
+        <img :src="item.img_src" />
+      </div>
+      <div class="car_right">
+        <p v-text="item.title"></p>
+        <p v-text="item.spec"></p>
+        <div class="car_buy">
+          <p><span>￥</span>{{item.price}}</p>
+          <VantStepper v-model="item.buyNum" integer min="1" button-size="0.5rem" input-width="0.8rem" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { Checkbox, Stepper } from 'vant'
+export default {
+  name: 'cartProduct',
+  data: () => {
+    return {
+      checked: false
+    }
+  },
+  components: {
+    VantCheckbox: Checkbox,
+    VantStepper: Stepper
+  },
+  props: {
+    item: {
+      type: Object
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+  .car_product {
+    display: flex;
+    justify-content: space-between;
+    background-color: #fff;
+    margin: 0.24rem;
+    padding: 0.24rem;
+    margin-bottom: 0.16rem;
+    border-radius: 0.1rem;
+    /deep/ .van-checkbox__icon .van-icon {
+      font-size: 0.24rem;
+    }
+    .car_box {
+      display: flex;
+      margin-left: 0.2rem;
+      .car_left {
+        img {
+          width: 2rem;
+          height: 2rem;
+        }
+      }
+      .car_right {
+        width: 3.4rem;
+        margin-left: 0.3rem;
+        font-size: 0.24rem;
+        p {
+          margin-bottom: 0.1rem;
+        }
+        p:first-child {
+          overflow: hidden;
+          text-overflow: ellipsis;
+          display:-webkit-box;
+          -webkit-box-orient:vertical;
+          -webkit-line-clamp:2;
+        }
+        .car_buy {
+          display: flex;
+          justify-content: space-between;
+          /deep/ .van-stepper__input {
+            font-size: 0.24rem;
+          }
+          p {
+            margin-top: 0.12rem;
+            color: #f75f47;
+            font-size: 0.3rem;
+            span {
+              font-size: 0.22rem;
+            }
+          }
+        }
+      }
+    }
+  }
+</style>
--- vue-demo/src/pages/Cart/index.vue
+++ vue-demo/src/pages/Cart/index.vue
@@ -1,26 +1,132 @@
 <template>
-  <div></div>
+  <div class="car_container">
+    <div class="car_top">
+      <div class="top_left">
+        <i class="iconfont icon-icon-shop"></i>
+        <span>乐町Online商城</span>
+      </div>
+      <div class="top_right">
+        <span @click="switchEdit">{{isEdit ? '完成' : '编辑'}}</span>
+      </div>
+    </div>
+    <div class="car_content">
+      <CartProduct v-for="(item, idx) in cartList" :key="idx" :item="item" />
+    </div>
+    <div class="car_bottom">
+      <div class="bottom_left">
+        <VantCheckbox />
+        <p>全选</p>
+      </div>
+      <div class="bottom_right">
+        <div class="left_all" v-if="!isEdit">
+          <p>合计: <span>￥</span><span>0</span></p>
+          <p>不含运费</p>
+        </div>
+        <div class="right_btn">
+          <span>{{isEdit ? '删除' : '结算'}}</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import {mapGetters, mapActions} from 'vuex'
+import {Checkbox} from 'vant'
+import CartProduct from './cartProduct'
 export default {
   name: 'Cart',
   data: () => {
     return {
+      isEdit: false
     }
   },
-  components: {},
+  components: {
+    CartProduct,
+    VantCheckbox: Checkbox
+  },
   async mounted () {
     await this.getCartProduct()
-    console.log(789789, this.cartList)
+  },
+  watch: {
   },
   computed: {
     ...mapGetters('Cart', ['cartList'])
   },
   methods: {
-    ...mapActions('Cart', ['getCartProduct'])
+    ...mapActions('Cart', ['getCartProduct']),
+    switchEdit () {
+      this.isEdit = !this.isEdit
+    }
   }
 }
 </script>
 <style lang="less" scoped>
+  .car_container {
+    font-size: 0.26rem;
+    background-color: #f6f6f6;
+    height: 100vh;
+    position: relative;
+    .car_top {
+      display: flex;
+      justify-content: space-between;
+      padding: 0.24rem;
+      background-color: #fff;
+      .top_left {
+        .iconfont {
+          font-size: 0.28rem;
+        }
+      }
+    }
+    .car_bottom {
+      position: fixed;
+      bottom: 1rem;
+      left: 0px;
+      width: 100vw;
+      height: 1rem;
+      padding: 0.1rem;
+      background-color: #fff;
+      display: flex;
+      justify-content: space-between;
+      .bottom_left {
+        display: flex;
+        margin-top: -0.15rem;
+        /deep/ .van-checkbox__icon .van-icon {
+          font-size: 0.28rem;
+        }
+        p {
+          margin-top: 0.38rem;
+          margin-left: 0.1rem;
+        }
+      }
+      .bottom_right {
+        display: flex;
+        .left_all {
+          margin-right: 0.2rem;
+          p:first-child {
+            font-weight: bold;
+            span {
+              font-size: 0.3rem;
+              color: #f75f47;
+            }
+            span:first-child {
+              font-size: 0.24rem;
+            }
+          }
+          p:last-child {
+            font-size: 0.22rem;
+            margin-top: 0.08rem;
+            color: rgba(0, 0, 0, .65);
+          }
+        }
+        .right_btn {
+          margin-top: 0.15rem;
+          span {
+            padding: 0.2rem 0.6rem;
+            border-radius: 0.4rem;
+            background-color: #ccc;
+          }
+        }
+      }
+    }
+  }
 </style>
